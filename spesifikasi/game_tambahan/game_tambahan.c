d199bcfb532cdf9ff132c44e44635145b47041ad
ZeevHaydar <nzhifhaidar@gmail.com>
2022-10-31 00:31:58 +0700

    nambah game tambahan
    cek dong kalo ada yg salah

---
+++ spesifikasi/game_tambahan/game_tambahan.c
@@ -0,0 +1,6 @@
+# include "game_tambahan.h"
+
+void GameTambahan() {
+    int random = rand();
+    printf("Permainan Selesai, Skor: %d", random);
+}
---
+++ spesifikasi/game_tambahan/game_tambahan.h
@@ -0,0 +1,13 @@
+#ifndef gametambahan_H
+#define gametambahan_H
+
+# include <stdio.h>
+# include <stdlib.h>
+
+void GameTambahan();
+/*
+I.S. Sembarang,
+F.S. menyelesaikan game dengan skor berupa random number
+*/
+
+#endif
