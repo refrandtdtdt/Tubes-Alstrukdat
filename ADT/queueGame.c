b4955053d6a427eb4116f05515e1f22435b2be02
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2022-11-02 15:30:53 +0700

    Commit acikiwir

---
+++ ADT/queueGame.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "queueGame.h"
+
+void tambahQueueGame ();
+/*
+Prosedur ini akan membaca nomor game yang akan dimasukkan ke dalam antrian, lalu dicek apakah nomor game
+tersebut masih dalam rentang nomor game yang ada pada daftar game.
+Asumsi : Queue game maksimal hanya menampung 100 antrian game
+I.S. Terdapat queue untuk menampung antrian.
+F.S. Apabila nomor game valid, game dengan nomor game tersebut dimasukkan ke dalam queue tempat penampungan,
+apabila tidak, akan mengeluarkan output bahwa nomor game tidak valid dan akan mengulang proses input nomor game
+yang akan dimasukkan ke queue tempat penampungan.
+*/
+
+/* *** Kreator *** */
+void CreateQueue(Queue *q);
+/* I.S. sembarang */
+/* F.S. Sebuah q kosong terbentuk dengan kondisi sbb: */
+/* - Index head bernilai IDX_UNDEF */
+/* - Index tail bernilai IDX_UNDEF */
+/* Proses : Melakukan alokasi, membuat sebuah q kosong */
+
+/* ********* Prototype ********* */
+boolean isEmpty(Queue q);
+/* Mengirim true jika q kosong: lihat definisi di atas */
+boolean isFull(Queue q);
+/* Mengirim true jika tabel penampung elemen q sudah penuh */
+/* yaitu IDX_TAIL akan selalu di belakang IDX_HEAD dalam buffer melingkar*/
+
+int length(Queue q);
+/* Mengirimkan banyaknya elemen queue. Mengirimkan 0 jika q kosong. */
+
+/* *** Primitif Add/Delete *** */
+void enqueue(Queue *q, ElType val);
+/* Proses: Menambahkan val pada q dengan aturan FIFO */
+/* I.S. q mungkin kosong, tabel penampung elemen q TIDAK penuh */
+/* F.S. val menjadi TAIL yang baru, IDX_TAIL "mundur" dalam buffer melingkar. */
+
+void dequeue(Queue *q, ElType *val);
+/* Proses: Menghapus val pada q dengan aturan FIFO */
+/* I.S. q tidak mungkin kosong */
+/* F.S. val = nilai elemen HEAD pd I.S., IDX_HEAD "mundur";
+        q mungkin kosong */
+
+/* *** Display Queue *** */
+void displayQueue(Queue q);
+/* Proses : Menuliskan isi Queue dengan traversal, Queue ditulis di antara kurung 
+   siku; antara dua elemen dipisahkan dengan separator "koma", tanpa tambahan 
+   karakter di depan, di tengah, atau di belakang, termasuk spasi dan enter */
+/* I.S. q boleh kosong */
+/* F.S. Jika q tidak kosong: [e1,e2,...,en] */
+/* Contoh : jika ada tiga elemen bernilai 1, 20, 30 akan dicetak: [1,20,30] */
+/* Jika Queue kosong : menulis [] */
