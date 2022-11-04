701df575795101eb47ceb7b0975a833d28ce49ab
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2022-11-04 16:28:45 +0700

    Commit gan

---
+++ ADT_Command/playGame.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "playGame.h"
+
+void mainkanGame (Queue * antrian_game) {
+    
+}
+/*
+Prosedur ini akan langsung memainkan game yang terletak pada antrian pertama pada antrian game
+I.S. Sudah tersedia antrian game di main program yang sudah terdefinisi
+F.S. Kondisi 1) Apabila game dalam antrian pertama bisa dimainkan, maka akan mengeluarkan output sedang loading 
+                game, lalu memainkan game. Game yang dimainkan akan dikeluarkan dari atrian game
+     Kondisi 2) Apabila game dalam antrian pertama tidak bisa dimainkan (dalam maintenance), maka akan mengeluarkan
+                output bahwa tidak bisa memainkan game. Game yang tidak bisa dimainkan tersebut dikeluarkan dari
+                antrian game
+*/
