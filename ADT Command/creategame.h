50a5c5c44c5d7088c02b33bd8068570a3a2e3723
victoria-angelique <91114869+victoria-angelique@users.noreply.github.com>
2022-11-10 01:02:00 +0700

    Add files via upload

---
+++ ADT Command/creategame.h
@@ -0,0 +1,15 @@
+/*File creategame.h*/
+
+#ifndef CREATEGAME_H
+#define CREATEGAME_H
+
+#include "../ADT DEFAULT/array.h"
+
+void buatgame (TabGame *T); 
+/*
+prosedur ini akan membaca masukkan nama game yang ingin ditambahkan ke dalam Binomo
+I.S Sembarang
+F.S Game berhasil ditambahkan ke dalam array game yang tersedia 
+Apabila game yg ditambahkan sudah ada di dalam array game sebelumnya maka game tidak ditambahkan kembali
+*/
+#endif
---
+++ ADT Command/listgame.h
@@ -0,0 +1,16 @@
+/*File : listgame.h*/
+
+#ifndef LISTGAME_H
+#define LISTGAME_H
+#include "../ADT DEFAULT/array.h"
+
+void ListGame (TabGame T);
+/* proses : menuliskan list atau daftar game yang terdapat dalam Binomo */
+/* I.S T boleh kosong */
+/* F.S Jika T tidak kosong indeks dan nama game ditulis berderet ke bawah
+Contoh output
+1. RNG 
+2. LUNCH SLOW
+3. DINOSAUR IN EARTH */
+/* Jika T kosong : Hanya Menulis "Tidak ada game yang tersimpan "*/
+#endif 
