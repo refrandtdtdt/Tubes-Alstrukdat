fddfc48edcc0be7bbb19cf89ee72337b080f0f0a
victoria-angelique <victoriaangelique01@gmail.com>
2022-11-11 02:55:32 +0700

    create drive

---
+++ _src/ADT/drivermesinkar.c
@@ -0,0 +1,12 @@
+#include "mesinkar.c"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (){
+    START(); /*menerima masukkan input*/
+    while (!eop)
+    {
+        printf("%c",cc);
+    }  
+}
