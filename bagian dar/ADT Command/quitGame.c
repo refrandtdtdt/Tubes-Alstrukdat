6056ff9a9032f6add0022fc1b7851bef20898af4
ZeevHaydar <nzhifhaidar@gmail.com>
2022-11-11 01:38:20 +0700

    Add Quit Driver
    Reyhan Be laik: "commit gan"

---
+++ bagian dar/ADT Command/quitGame.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "quitGame.h"
+
+void Quit() {
+    printf("keluar dari Game\n");
+    printf("Jangan lupa tidur cukup dan makan makanan yang bergizi\n");
+    printf("Sayounara\n");
+    exit(0);
+}
---
+++ bagian dar/Driver/quitgameDriver.c
@@ -0,0 +1,6 @@
+# include "../ADT Command/quitGame.h"
+
+int main()  {
+    Quit();
+    return 0;
+}
