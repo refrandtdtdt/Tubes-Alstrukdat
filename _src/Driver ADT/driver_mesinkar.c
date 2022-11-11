f44bb7cf4d4130750025f277c03e7228484030ba
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2022-11-11 19:15:28 +0700

    Commit

---
+++ _src/Driver ADT/driver_mesinkar.c
@@ -0,0 +1,36 @@
+# include <stdio.h>
+# include "../ADT/mesinkar.c"
+# include "../ADT/functions.c"
+
+int main()
+{
+    /* *** Membaca File *** */
+    // START Mesin Karakter
+    STARTCHAR("pitakar.txt");
+
+    // Check First Character
+    char first = GetCCCHAR();
+    printf("Huruf pertama: %c\n", first);
+
+    // Check ADVCHAR() 
+    ADVCHAR();
+    char second = GetCCCHAR();
+    printf("Huruf kedua: %c\n", second);
+    
+    // Check if EOP
+    printf("%d\n", IsEOPCHAR());
+
+    // Print semua character dari awal sampaii EOP
+    CHARMACHINE("pitakar.txt");
+    printf("\n");
+    
+    // Check if EOP is true
+    printf("%d\n", IsEOPCHAR());  
+
+    // Check First Character
+    char firstC = GetCCCHAR();
+    printf("%c\n", firstC);
+
+    return 0;
+
+}
