f1bb9d14904c857558d9bf5b5bc333b5ff336db1
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2022-11-11 18:40:10 +0700

    Commit

--- _src/ADT/mesinkar.c
+++ _src/ADT/mesinkar.c
@@ -9,6 +9,41 @@ boolean file;
 FILE *pita;
 int retval;
 
+char cc;
+boolean eop;
+
+FILE *pita;
+int retval;
+
+boolean IsEOPCHAR()   {
+    return (cc == MARKCHAR);
+}
+
+void STARTCHAR(char* filename)    {
+    pita = fopen(filename, "r");
+    ADVCHAR();
+}
+
+void ADVCHAR()  {
+    retval = fscanf(pita, "%c", &cc);
+    eop = IsEOPCHAR();
+    if (eop)    {
+        fclose(pita);
+    }
+}
+
+char GetCCCHAR()    {
+    return cc;
+}
+
+void CHARMACHINE(char* filename)  {
+    STARTCHAR(filename);
+    while (!IsEOPCHAR())  {
+        printf("%c", cc);
+        ADVCHAR();
+    }
+}
+
 void ADV()
 {
    retval = fscanf(pita,"%c",&cc);
--- _src/ADT/mesinkar.h
+++ _src/ADT/mesinkar.h
@@ -7,6 +7,35 @@ extern char cc;
 extern boolean eop;
 extern boolean file;
 
+#define MARKCHAR '~'
+/* State Mesin */
+extern char currentChar;
+extern boolean EOPCHAR;
+
+void STARTCHAR(char* filename);
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   Pita baca diambil dari stdin.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
+
+void ADVCHAR();
+/* Pita dimajukan satu karakter.
+   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
+   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama,
+          currentChar mungkin = MARK
+          Jika  currentChar = MARK maka EOP akan menyala (true) */
+
+char GetCCCHAR();
+/* Mengirimkan currentChar */
+
+boolean IsEOPCHAR();
+/* Mengirimkan true jika currentChar = MARK */
+
+void CHARMACHINE(char* filename);
+
 char cc;
 boolean eop;
 boolean file;
