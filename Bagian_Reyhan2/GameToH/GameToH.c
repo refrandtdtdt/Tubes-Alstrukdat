6f282e2b5721220eee12416adb5ec85f46d61ca0
ReyhanPA <87368608+ReyhanPA@users.noreply.github.com>
2022-11-24 02:30:12 +0700

    Hampir kelar ToH

--- Bagian_Reyhan2/GameToH/GameToH.c
+++ Bagian_Reyhan2/GameToH/GameToH.c
@@ -107,205 +107,211 @@ void TowerOfHanoi() {
     while (!end) {
         if (isEqStack(tiangAcuan, tiangC)) {
             end = true;
-        }
-        // Tampilan stack
-        system("cls");
-        Display3Stack(tiangA, tiangB, tiangC);
-        for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-            printf("=");
-        }
-        printf("\n");
-        for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-            if (m == ((2 * intJumlahPiringan) / 2)) {
-                printf("A");
-            } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
-                printf("B");
-            } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
-                printf("C");
-            } else {
-                printf(" ");
+        } else {
+            // Tampilan stack
+            system("cls");
+            Display3Stack(tiangA, tiangB, tiangC);
+            for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                printf("=");
             }
-        }
-        printf("\n");
-        for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-            printf("=");
-        }
-
-        // Tiang asal
-        valid = false;
-        while (!valid) {
-            // Input tiang asal
             printf("\n");
-            printf("Masukkan pilihan anda!\n");
-            printf("Tiang asal : ");
-            START();
-            convertToArrayOfKata(&tiangAsal, 1);
-            if ((tiangAsal.Length == 1) && (tiangAsal.buffer[0].Length == 1) && (tiangAsal.buffer[0].TabWord[0] == 65 || tiangAsal.buffer[0].TabWord[0] == 97 || tiangAsal.buffer[0].TabWord[0] == 66 || tiangAsal.buffer[0].TabWord[0] == 98 || tiangAsal.buffer[0].TabWord[0] == 67 || tiangAsal.buffer[0].TabWord[0] == 99)) {
-                valid = true;
-            } else {
-                // Tampilan stack kalo salah input
-                system("cls");
-                Display3Stack(tiangA, tiangB, tiangC);
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    printf("=");
+            for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                if (m == ((2 * intJumlahPiringan) / 2)) {
+                    printf("A");
+                } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
+                    printf("B");
+                } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
+                    printf("C");
+                } else {
+                    printf(" ");
                 }
+            }
+            printf("\n");
+            for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                printf("=");
+            }
+
+            // Tiang asal
+            valid = false;
+            while (!valid) {
+                // Input tiang asal
                 printf("\n");
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    if (m == ((2 * intJumlahPiringan) / 2)) {
-                        printf("A");
-                    } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
-                        printf("B");
-                    } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
-                        printf("C");
-                    } else {
-                        printf(" ");
+                printf("Masukkan pilihan anda!\n");
+                printf("Tiang asal : ");
+                START();
+                convertToArrayOfKata(&tiangAsal, 1);
+                if ((tiangAsal.Length == 1) && (tiangAsal.buffer[0].Length == 1) && (tiangAsal.buffer[0].TabWord[0] == 65 || tiangAsal.buffer[0].TabWord[0] == 97 || tiangAsal.buffer[0].TabWord[0] == 66 || tiangAsal.buffer[0].TabWord[0] == 98 || tiangAsal.buffer[0].TabWord[0] == 67 || tiangAsal.buffer[0].TabWord[0] == 99)) {
+                    valid = true;
+                } else {
+                    // Tampilan stack kalo salah input
+                    system("cls");
+                    Display3Stack(tiangA, tiangB, tiangC);
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        printf("=");
                     }
+                    printf("\n");
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        if (m == ((2 * intJumlahPiringan) / 2)) {
+                            printf("A");
+                        } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
+                            printf("B");
+                        } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
+                            printf("C");
+                        } else {
+                            printf(" ");
+                        }
+                    }
+                    printf("\n");
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        printf("=");
+                    }
+                    printf("\n");
+                    printf("Masukan tidak valid!");
                 }
-                printf("\n");
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    printf("=");
+                
+            }
+
+            // Tiang tujuan
+            valid = false;
+            while (!valid) {
+                // Input tiang tujuan
+                printf("Tiang tujuan : ");
+                START();
+                convertToArrayOfKata(&tiangTujuan, 1);
+                if ((tiangTujuan.Length == 1) && (tiangTujuan.buffer[0].Length == 1) && (tiangTujuan.buffer[0].TabWord[0] == 65 || tiangTujuan.buffer[0].TabWord[0] == 97 || tiangTujuan.buffer[0].TabWord[0] == 66 || tiangTujuan.buffer[0].TabWord[0] == 98 || tiangTujuan.buffer[0].TabWord[0] == 67 || tiangTujuan.buffer[0].TabWord[0] == 99)) {
+                    valid = true;
+                } else {
+                    // Tampilan stack
+                    system("cls");
+                    Display3Stack(tiangA, tiangB, tiangC);
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        printf("=");
+                    }
+                    printf("\n");
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        if (m == ((2 * intJumlahPiringan) / 2)) {
+                            printf("A");
+                        } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
+                            printf("B");
+                        } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
+                            printf("C");
+                        } else {
+                            printf(" ");
+                        }
+                    }
+                    printf("\n");
+                    for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
+                        printf("=");
+                    }
+                    // Input tiang tujuan kalo salah
+                    printf("\n");
+                    printf("Masukan tidak valid!\n");
+                    printf("Masukkan pilihan anda!\n");
+                    printf("Tiang asal : %c\n", tiangAsal.buffer[0].TabWord[0]);
                 }
-                printf("\n");
-                printf("Masukan tidak valid!");
             }
             
-        }
-
-        // Tiang tujuan
-        valid = false;
-        while (!valid) {
-            // Input tiang tujuan
-            printf("Tiang tujuan : ");
-            START();
-            convertToArrayOfKata(&tiangTujuan, 1);
-            if ((tiangTujuan.Length == 1) && (tiangTujuan.buffer[0].Length == 1) && (tiangTujuan.buffer[0].TabWord[0] == 65 || tiangTujuan.buffer[0].TabWord[0] == 97 || tiangTujuan.buffer[0].TabWord[0] == 66 || tiangTujuan.buffer[0].TabWord[0] == 98 || tiangTujuan.buffer[0].TabWord[0] == 67 || tiangTujuan.buffer[0].TabWord[0] == 99)) {
-                valid = true;
-            } else {
-                // Tampilan stack
-                system("cls");
-                Display3Stack(tiangA, tiangB, tiangC);
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    printf("=");
+            // Pop piringan teratas dari tiangAsal
+            if (tiangAsal.buffer[0].TabWord[0] == 'A' || tiangAsal.buffer[0].TabWord[0] == 'a') {
+                i = Top(tiangA);
+                while ((tiangA.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangA, &dummyTiang);
+                    i--;
                 }
-                printf("\n");
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    if (m == ((2 * intJumlahPiringan) / 2)) {
-                        printf("A");
-                    } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * intJumlahPiringan - 1))) {
-                        printf("B");
-                    } else if (m == (((2 * intJumlahPiringan) / 2) + (2 * (2 * intJumlahPiringan - 1)))) {
-                        printf("C");
-                    } else {
-                        printf(" ");
-                    }
+                if (i >= 0) {
+                    Pop(&tiangA, &dummyTiangAsal);
                 }
-                printf("\n");
-                for (m = 1; m <= (2 * intJumlahPiringan - 1) * 3; m++) {
-                    printf("=");
+            } else if (tiangAsal.buffer[0].TabWord[0] == 'B' || tiangAsal.buffer[0].TabWord[0] == 'b') {
+                i = Top(tiangB);
+                while ((tiangB.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangB, &dummyTiang);
+                    i--;
                 }
-                // Input tiang tujuan kalo salah
-                printf("\n");
-                printf("Masukan tidak valid!\n");
-                printf("Masukkan pilihan anda!\n");
-                printf("Tiang asal : %c\n", tiangAsal.buffer[0].TabWord[0]);
+                if (i >= 0) {
+                    Pop(&tiangB, &dummyTiangAsal);
+                }
+            } else if (tiangAsal.buffer[0].TabWord[0] == 'C' || tiangAsal.buffer[0].TabWord[0] == 'c') {
+                i = Top(tiangC);
+                while ((tiangC.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangC, &dummyTiang);
+                    i--;
+                }
+                if (i >= 0) {
+                    Pop(&tiangC, &dummyTiangAsal);
+                }
             }
-        }
-        
-        // Pop piringan teratas dari tiangAsal
-        if (tiangAsal.buffer[0].TabWord[0] == 'A' || tiangAsal.buffer[0].TabWord[0] == 'a') {
-            i = Top(tiangA);
-            while ((tiangA.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangA, &dummyTiang);
-                i--;
-            }
-            if (i >= 0) {
-                Pop(&tiangA, &dummyTiangAsal);
-            }
-        } else if (tiangAsal.buffer[0].TabWord[0] == 'B' || tiangAsal.buffer[0].TabWord[0] == 'b') {
-            i = Top(tiangB);
-            while ((tiangB.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangB, &dummyTiang);
-                i--;
-            }
-            if (i >= 0) {
-                Pop(&tiangB, &dummyTiangAsal);
-            }
-        } else if (tiangAsal.buffer[0].TabWord[0] == 'C' || tiangAsal.buffer[0].TabWord[0] == 'c') {
-            i = Top(tiangC);
-            while ((tiangC.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangC, &dummyTiang);
-                i--;
-            }
-            if (i >= 0) {
-                Pop(&tiangC, &dummyTiangAsal);
-            }
-        }
 
-        // Isi kekosongan stack yang telah dipop dengan tiang
-        if (tiangAsal.buffer[0].TabWord[0] == 'A' || tiangAsal.buffer[0].TabWord[0] == 'a') {
-            j = intJumlahPiringan - (Top(tiangA) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangA, tiangDummy);
+            // Isi kekosongan stack yang telah dipop dengan tiang
+            if (tiangAsal.buffer[0].TabWord[0] == 'A' || tiangAsal.buffer[0].TabWord[0] == 'a') {
+                j = intJumlahPiringan - (Top(tiangA) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangA, tiangDummy);
+                }
+            } else if (tiangAsal.buffer[0].TabWord[0] == 'B' || tiangAsal.buffer[0].TabWord[0] == 'b') {
+                j = intJumlahPiringan - (Top(tiangB) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangB, tiangDummy);
+                }
+            } else if (tiangAsal.buffer[0].TabWord[0] == 'C' || tiangAsal.buffer[0].TabWord[0] == 'c') {
+                j = intJumlahPiringan - (Top(tiangC) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangC, tiangDummy);
+                }
             }
-        } else if (tiangAsal.buffer[0].TabWord[0] == 'B' || tiangAsal.buffer[0].TabWord[0] == 'b') {
-            j = intJumlahPiringan - (Top(tiangB) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangB, tiangDummy);
-            }
-        } else if (tiangAsal.buffer[0].TabWord[0] == 'C' || tiangAsal.buffer[0].TabWord[0] == 'c') {
-            j = intJumlahPiringan - (Top(tiangC) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangC, tiangDummy);
-            }
-        }
 
-        // Pop piringan teratas dari tiangTujuan dan masukkan dummyTiangAsal
-        if (tiangTujuan.buffer[0].TabWord[0] == 'A' || tiangTujuan.buffer[0].TabWord[0] == 'a') {
-            i = Top(tiangA);
-            while ((tiangA.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangA, &dummyTiang);
-                i--;
+            // Pop piringan teratas dari tiangTujuan dan masukkan dummyTiangAsal
+            if (tiangTujuan.buffer[0].TabWord[0] == 'A' || tiangTujuan.buffer[0].TabWord[0] == 'a') {
+                i = Top(tiangA);
+                while ((tiangA.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangA, &dummyTiang);
+                    i--;
+                }
+                Push(&tiangA, dummyTiangAsal);
+            } else if (tiangTujuan.buffer[0].TabWord[0] == 'B' || tiangTujuan.buffer[0].TabWord[0] == 'b') {
+                i = Top(tiangB);
+                while ((tiangB.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangB, &dummyTiang);
+                    i--;
+                }
+                Push(&tiangB, dummyTiangAsal);
+            } else if (tiangTujuan.buffer[0].TabWord[0] == 'C' || tiangTujuan.buffer[0].TabWord[0] == 'c') {
+                i = Top(tiangC);
+                while ((tiangC.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
+                    Pop(&tiangC, &dummyTiang);
+                    i--;
+                }
+                Push(&tiangC, dummyTiangAsal);
             }
-            Push(&tiangA, dummyTiangAsal);
-        } else if (tiangTujuan.buffer[0].TabWord[0] == 'B' || tiangTujuan.buffer[0].TabWord[0] == 'b') {
-            i = Top(tiangB);
-            while ((tiangB.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangB, &dummyTiang);
-                i--;
-            }
-            Push(&tiangB, dummyTiangAsal);
-        } else if (tiangTujuan.buffer[0].TabWord[0] == 'C' || tiangTujuan.buffer[0].TabWord[0] == 'c') {
-            i = Top(tiangC);
-            while ((tiangC.T[i].TabWord[intJumlahPiringan-1] == '|') && (i >= 0)) {
-                Pop(&tiangC, &dummyTiang);
-                i--;
-            }
-            Push(&tiangC, dummyTiangAsal);
-        }
 
-        // Isi kekosongan stack yang telah dipop dengan tiang
-        if (tiangTujuan.buffer[0].TabWord[0] == 'A' || tiangTujuan.buffer[0].TabWord[0] == 'a') {
-            j = intJumlahPiringan - (Top(tiangA) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangA, tiangDummy);
+            // Isi kekosongan stack yang telah dipop dengan tiang
+            if (tiangTujuan.buffer[0].TabWord[0] == 'A' || tiangTujuan.buffer[0].TabWord[0] == 'a') {
+                j = intJumlahPiringan - (Top(tiangA) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangA, tiangDummy);
+                }
+            } else if (tiangTujuan.buffer[0].TabWord[0] == 'B' || tiangTujuan.buffer[0].TabWord[0] == 'b') {
+                j = intJumlahPiringan - (Top(tiangB) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangB, tiangDummy);
+                }
+            } else if (tiangTujuan.buffer[0].TabWord[0] == 'C' || tiangTujuan.buffer[0].TabWord[0] == 'c') {
+                j = intJumlahPiringan - (Top(tiangC) + 1);
+                for (i = 0; i < j; i++) {
+                    Push(&tiangC, tiangDummy);
+                }
             }
-        } else if (tiangTujuan.buffer[0].TabWord[0] == 'B' || tiangTujuan.buffer[0].TabWord[0] == 'b') {
-            j = intJumlahPiringan - (Top(tiangB) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangB, tiangDummy);
-            }
-        } else if (tiangTujuan.buffer[0].TabWord[0] == 'C' || tiangTujuan.buffer[0].TabWord[0] == 'c') {
-            j = intJumlahPiringan - (Top(tiangC) + 1);
-            for (i = 0; i < j; i++) {
-                Push(&tiangC, tiangDummy);
-            }
-        }
 
-        // Mengosongkan dummyTiang
-        for (j = 0; j < ((2 * intJumlahPiringan) - (2 * i + 1)); j++) {
-            dummyTiang.TabWord[j] = '\0';
+            // Mengosongkan dummyTiang
+            for (j = 0; j < ((2 * intJumlahPiringan) - (2 * i + 1)); j++) {
+                dummyTiang.TabWord[j] = '\0';
+            }
         }
     }
+
+    // Game ToH telah selesai, menampilkan score
+    system("cls");
+    printf("Selamat, kamu telah memenangkan game ini!\n");
+    printf("Score kamu : \n");
 }
 
 int main() {
