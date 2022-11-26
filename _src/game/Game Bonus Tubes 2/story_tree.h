3bcb4ea54eb813d44595928b00c6e13b4873759b
ZeevHaydar <nzhifhaidar@gmail.com>
2022-11-26 14:02:30 +0700

    Update story_tree.h
    uwu

--- _src/game/Game Bonus Tubes 2/story_tree.h
+++ _src/game/Game Bonus Tubes 2/story_tree.h
@@ -1,16 +1,22 @@
 #ifndef STORY_TREE_H
 #define STORY_TREE_H
 
+#include "..\..\boolean.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
 /*Selektor*/
 #define ROOT(p)  (p)->info
+#define INFO(p)  (p)->info
 #define LEFT(p)  (p)->left
 #define RIGHT(p) (p)->right
 
 typedef struct StoryNode* Address;
 /* Definisi Pohon Biner */
-/* pohon biner kosong p = NIL*/
+/* pohon biner kosong p = NULL*/
 typedef struct StoryTreeNode   {
-    int id;
+    int info;
     Address left;
     Address right;
 } StoryNode;
@@ -19,4 +25,30 @@ typedef Address StoryTree;
 
 /*Konstruktor*/
 
+/* I.S Sembarang*/
+/* F.S Menghasilkan sebuah pohon p*/
+/* Menghasilkan sebuah pohon biner p dari akar, l, dan r, jika alokasi berhasil*/
+/* Menghasilkan pohon p yang kosong (NULL) jika alokasi gagal*/
+void CreateStoryTree(int akar, StoryTree l, StoryTree r, StoryTree* p);
+
+/* Mengirimkan address hasil alokasi sebuah elemen bernilai x*/
+/* Jika alokasi berhasil, maka address tidak NULL, dan misalnya menghasilkan p,
+maka INFO(p) = x, LEFT(p) = NULL, RIGHT(p) = NULL*/
+/* Jika Alokasi gagal, mengirimkan NULL*/
+Address newStoryNode(int x);
+
+/*Mengirimkan true jika p adalah pohon yang kosong*/
+boolean IsTreeEmpty (StoryTree p);
+
+/* Mengirimkan true jika p adalah pohon yang tidak kosong dan terdiri dari 1 elemen saja */ 
+boolean IsOneElement(StoryTree p);
+
+/* Mengirimkan true jike p adalah pohon Uner Left (hanya mempunyai subpohon kiri) */
+boolean IsUnerLeft(StoryTree p);
+
+/* Mengirimkan true jika p adalah pohon Uner Right (hanya mempunyai subpohon kanan) */
+boolean IsUnerRight(StoryTree p);
+
+/* Mengirimkan true jika p adalah pohon biner*/
+boolean IsBiner(StoryTree p);
 #endif
