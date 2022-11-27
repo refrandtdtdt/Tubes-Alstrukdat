9c744429815b159fc843a63c8faa805c12ba7353
ZeevHaydar <nzhifhaidar@gmail.com>
2022-11-27 14:38:36 +0700

    Ngelanjutin Story-Node 1
    uwu

---
+++ _src/game/Game Bonus Tubes 2/game_over_node.h
@@ -0,0 +1,12 @@
+#ifndef GAME_OVER
+#define GAME_OVER
+
+#include "story_tree.h"
+#include "boolean.h"
+
+void game_over_node(boolean *isRunning)   {
+    printf("YOU LOSE");
+    *isRunning = false;
+}
+
+#endif
--- _src/game/Game Bonus Tubes 2/story1_node.h
+++ _src/game/Game Bonus Tubes 2/story1_node.h
@@ -1,13 +1,52 @@
 #include "story_tree.h"
+#include "game_over_node.h"
 
-void node1(int *score)
+void node1(int *score, char* player)
 {
+    boolean running = true;
     char dummy;
-    printf("\"Nanti kita sekolah bareng ya!!!\"");scanf("%c", &dummy);printf("\n\n");
-    printf("aku tiba-tiba teringat kembali ke masa itu.");scanf("%c", &dummy);printf("\n");
-    printf("tumpukan debu meliputi buku tersebut. aku segera mengambil kain untuk membersihkan cover buku tersebut.");scanf("%c", &dummy);printf("\n");
-    printf("Aku sudah lama tidak menulis kembali diariku.");scanf("%c", &dummy);printf("\n");
-    printf("mungkin aku sudah bosan dengan hal itu.")scanf("%c", &dummy);printf("\n");
-    printf("kubuka halaman depan buku itu, kubaca kembali kenangan masa Sekolah Dasarku, tertulis pengalamanku bersama Miya.");scanf("%c", &dummy);printf("\n\n");
-    printf("aku dan Miya adalah teman dari masa kecil. dulu kami selalu bermain bersama.");scanf("%c", &dummy);printf("\n");
+    while(running)  {
+        printf("\"Nanti kita sekolah bareng ya!!!\"");scanf("%c", &dummy);system("cls");
+        printf("aku tiba-tiba teringat kembali ke masa itu.");scanf("%c", &dummy);system("cls");
+        printf("tumpukan debu meliputi buku tersebut. aku segera mengambil kain untuk membersihkan cover buku tersebut.");scanf("%c", &dummy);system("cls");
+        printf("Aku sudah lama tidak menulis kembali diariku.");scanf("%c", &dummy);system("cls");
+        printf("mungkin aku sudah bosan dengan hal itu.")scanf("%c", &dummy);system("cls");
+        printf("kubuka halaman depan buku itu, kubaca kembali kenangan masa Sekolah Dasarku, tertulis pengalamanku bersama Miya.");scanf("%c", &dummy);system("cls");
+        printf("aku dan Miya adalah teman dari masa kecil. dulu kami selalu bermain bersama.");scanf("%c", &dummy);system("cls");
+        printf("perkenalan awalku dimulai ketika Bu Kumakawa datang ke tempatku. ia membawa Miya sambil memperkenalkan diri.");scanf("%c", &dummy);system("cls");
+        printf("Bu Kumakawa: \"Tolong kenalin dirimu ya, Miya-chan.\"");scanf("%c", &dummy);system("cls");
+        printf("Miya: \"Baik, Okaa-san\"");scanf("%c", &dummy);system("cls");
+        printf("Miya: \"Halo, Namaku Miya, Kumakawa Miya. Salam Kenal\"");scanf("%c", &dummy);system("cls");
+        printf("%s: \"oke, namaku %s, salam kenal ya!\"", player);scanf("%c", &dummy);system("cls");
+        printf("Miya: \"Salam kenal, %s-San.\"");scanf("%c", &dummy);system("cls");
+        printf("semenjak itu, kita semakin akrab. aku dan dia menikmati waktu bersama bermain di taman bermain dekat rumah,
+         bermain petak umpet bersama teman-temanku yang lain, belajar matematika bareng di rumahku, kebetulan juga ayahku seorang guru matematika. 
+         dan kami juga bersekolah di sekolah yang sama.");scanf("%c", &dummy);system("cls");
+        printf("saat kelulusan, kami membuat janji bersama.");scanf("%c", &dummy);system("cls");
+        printf("Miya: \"Nanti kita masuk ke sekolah yang sama ya!!!\"");scanf("%c", &dummy);system("cls");
+        printf("kami berusaha menepatinya bersama.");scanf("%c", &dummy);system("cls");
+        printf("namun sayang hingga saat tes masuk ke SMP diumumkan, ternyata nilaiku masih kurang untuk masuk ke SMP favorit yang jadi pilihan Miya, 
+        akhirnya kami berpisah saat itu karena aku pindah kota untuk mencari sekolah terbaik lain yang ada di luar kota.
+        kami pun akhirnya jarang bertemu semenjak itu.");scanf("%c", &dummy);system("cls");
+        printf("Masa awal SMA pun dimulai, aku memulai kehidupan SMA biasaku.");scanf("%c", &dummy);system("cls");
+        printf("aku melihat palang kelas di atasku, sembari mencari kelasku. maklum ini hari pertamaku memasuki SMA.");scanf("%c", &dummy);system("cls");
+        printf("akhirnya aku melihatnya, \"kelas 10-2\".");scanf("%c", &dummy);system("cls");
+        printf("Aku kemudian memasuki ruang kelas itu.");scanf("%c", &dummy);system("cls");
+        printf("kupandangi wajah-wajah teman-temanku. beberapa ada yang kuingat karena merupakan teman sekelasku pada masa SD.");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+        printf("");scanf("%c", &dummy);system("cls");
+    }
+
+
 }
