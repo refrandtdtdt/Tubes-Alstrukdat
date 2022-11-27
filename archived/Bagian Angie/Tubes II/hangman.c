36584dce6ad7671c3be8b8b523f6dac1eb4c71e3
victoria-angelique <victoriaangelique01@gmail.com>
2022-11-27 20:14:00 +0700

    hangmanhangman

--- archived/Bagian Angie/Tubes II/hangman.c
+++ archived/Bagian Angie/Tubes II/hangman.c
@@ -36,19 +36,19 @@ void hangman ()
         length = strlen(kata);
         while (kesempatan != 0) // kesempatan tidak habis
         {
-            found = 0;
             printf("Tebakan sebelumnya: \n");
             printf("Kata: ");
+            srand(0);
+            printf("%s", listKata.TK[rand()%10].TabWord);
             for (i =0 ; i < length; i++)
             {
-                printf(&listKata[rand()%10]); 
                 printf("Kesempatan: ", kesempatan);
                 kesempatan -= 1;
                 printf("Masukan tebakan: ");
                 char *tebakan; 
                 boolean found = true;
                 START(); // membaca tebakan dari penggunakan
-                if (tebakan != T.TK[i])
+                if (!Eqstr(tebakan, T.TK[i].TabWord))
                 {
                     found = false;
                     if (!found)
@@ -72,37 +72,29 @@ void hangman ()
 void Load(char *filename, TabKata *list)
 {
     char* currline;
-    boolean exists = STARTF(filename);
-    if(!exists)
+    STARTF(filename);
+    Sentence bacaKata; CreateSentence(&bacaKata);
+    int kata = 0;
+    while (cc != '\n')
     {
-        printf("\nSavefile could not be found\n");
-    }
-    else
-    {
-        Sentence bacaKata; CreateSentence(&bacaKata);
-        int kata = 0;
-        while (cc != '\n')
-        {
-            kata *= 10;
-            kata += cc-'0';
-            ADV();
-        }
-        printf("%d games loaded\n", kata);
+        kata *= 10;
+        kata += cc-'0';
         ADV();
-        for (int i = 0; i < kata; i++)
-        {
-            clear(bacaKata.buffer[i].TabWord);
-        }
-        convertToArrayOfKata(&bacaKata, kata);
-        for (int i = 0; i <kata;i++)
-        {
-            list->TK[i] = bacaKata.buffer[i];
-        }
-        list->Neff = kata;
-        printf("\nSavefile loaded successfully\n");
-        CLOSEF();
     }
+    ADV();
+    for (int i = 0; i < kata; i++)
+    {
+        clear(bacaKata.buffer[i].TabWord);
+    }
+    convertToArrayOfKata(&bacaKata, kata);
+    for (int i = 0; i <kata;i++)
+    {
+            list->TK[i] = bacaKata.buffer[i];
+    }
+    list->Neff = kata;
+    CLOSEF();
 }
+
 void Save(char* filename, TabKata list)
 {
     STARTW(filename);
