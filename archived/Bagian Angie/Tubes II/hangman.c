9ace391cac7a622a7c9c71ab11f4d0eea8c22b35
victoria-angelique <victoriaangelique01@gmail.com>
2022-11-30 00:18:30 +0700

    halo ini hangman

--- archived/Bagian Angie/Tubes II/hangman.c
+++ archived/Bagian Angie/Tubes II/hangman.c
@@ -1,106 +1,32 @@
 #include "hangman.h"
 #include "mesinkata_modif.h"
-#include "listword.h"
 #include <stdlib.h>
 #include "array.h"
 #include "functions.h"
-
-void hangman ()
-{
-    printf("  __   __  _______  __    _  _______  __   __  _______  __    _ \n");
-    printf("|  | |  ||   _   ||  |  | ||       ||  |_|  ||   _   ||  |  | |\n");
-    pritnf("|  |_|  ||  |_|  ||   |_| ||    ___||       ||  |_|  ||   |_| |\n");
-    printf("|       ||       ||       ||   | __ |       ||       ||       |\n");
-    printf("|       ||       ||       ||   | __ |       ||       ||       |\n");
-    printf("|       ||       ||  _    ||   ||  ||       ||       ||  _    |\n");
-    printf("|   _   ||   _   || | |   ||   |_| || ||_|| ||   _   || | |   |\n");
-    printf("|__| |__||__| |__||_|  |__||_______||_|   |_||__| |__||_|  |__|n");
-    printf("\n");
-    printf("                      |***********************|\n");
-    printf("                      |       MAIN  MENU      |\n");
-    printf("                      |***********************|\n");
-    printf("                      |          PLAY         |\n");
-    printf("                      |         ADD WORD      |\n");
-    printf("                      |***********************|\n");
-    printf("\n");
-    Sentence input,tebakan;
-    TabKata listKata; MakeEmpty(&listKata);
-    printf("Choose Menu: ");
-    START();
-    convertToArrayOfKata(&input,4);
-    int length;
-    int i = 0;
-    if (Eqstr(&input,"PLAY"))
-    {
-        Load("kata.txt",&listKata);
-        int kesempatan = 10;
-        Sentence input,tebakan;
-        int i;
-        int length;
-        length = len(&listKata);
-        while (kesempatan != 0)
-        {
-            printf("Tebakan sebelumnya: %s",tebakan); // masukkin input stlahnya cara nampilin - gman ya
-            printf("Kesempatan: %d",kesempatan);
-            srand(0);
-            Word soal;
-            CreateWord(&soal);
-            for (i = 0; i <= length-1; i++)
-            {
-                (*soal).TabWord[i] = (&listKata).TabWord[i];
-            }
-            soal = &listKata[rand()%5].TK;
-            for (i; i<length; i++)
-                printf("_", soal.TabWord); // menampilkan _ _ _ _
-            printf("Masukkan Tebakan: ");
-            START();
-            convertToArrayOfKata(&tebakan,1);//input satu huruf tebakan
-            kesempatan -= 1;
-            if (&tebakan == soal.TabWord[i]) // cara akses satu persatu hurufny gmana ya 
-            {
-                printf("%s",soal.TabWord[i]); // nampilin huruf yang uda ketebak 
-                // cek apakah uda sama semua hurufnya
-                if (!Eqstr(&tebakan,soal.TabWord[i]))
-                {
-                    printf("Berhasil menebak kata %s",&soal,"Kamu mendapatkan %d ", length, " poin");
-                }
-            }
-        }
-    }
-    else if(Eqstr(&input, "ADD WORD"))
-    {
-        Load("kata.txt",&listKata);
-        printf("MAsukkan kata baru yang ingin ditambahkan: ");
-        tambahkata(&listKata);
-    }
-    else
-    {
-        printf("\nCommand tidak dikenali, silahkan masukkan command yang valid.\n");
-    }
-}
+#include "time.h"
 void Load(char *filename, TabKata *list)
 {
     char* currline;
-    STARTF(filename);
-    Sentence bacaKata; CreateSentence(&bacaKata);
-    int kata = 0;
+    boolean exists = STARTF(filename);
+    Sentence listkata; CreateSentence(&listkata);
+    int banyakkata = 0;
     while (cc != '\n')
     {
-        kata *= 10;
-        kata += cc-'0';
-        ADV();
+        banyakkata *= 10;
+        banyakkata += cc-'0';
+        ADV();        
     }
     ADV();
-    for (int i = 0; i < kata; i++)
+    for (int i = 0; i < banyakkata; i++)
     {
-        clear(bacaKata.buffer[i].TabWord);
+        clear(listkata.buffer[i].TabWord);
     }
-    convertToArrayOfKata(&bacaKata, kata);
-    for (int i = 0; i <kata;i++)
+    convertToArrayOfKata(&listkata, banyakkata);
+    for (int i = 0; i <banyakkata;i++)
     {
-            list->TK[i] = bacaKata.buffer[i];
+        list->TK[i] = listkata.buffer[i];
     }
-    list->Neff = kata;
+    list->Neff = banyakkata;
     CLOSEF();
 }
 
@@ -115,6 +41,142 @@ void Save(char* filename, TabKata list)
     CLOSEF();
 }
 
+void tambahkata (TabKata *T)
+{
+    printf("Masukkan kata baru yang akan ditambahkan: ");
+    START();
+    file = true;
+    CopyWord();
+    boolean isMember = false;
+    int i;
+    while (!isMember && i<=GetLastIdx(*T))
+    {
+        if (Eqstr((*T).TK[i].TabWord, kataToString(currentWord)))
+        {
+            isMember = true;
+        }
+        else
+        {
+            isMember = false;
+        }
+        i++;
+    }
+    if (!isMember) //bukan member 
+    {
+        (*T).Neff++;
+        (*T).TK[i] = currentWord;
+        printf("Kata berhasil ditambahkan\n");
+    }
+    else 
+    {
+        printf("Kata sudah terdaftar\n");
+    }
+}
+
+void hangman ()
+{
+    printf("  __   __  _______  __    _  _______  __   __  _______  __    _ \n");
+    printf("|  | |  ||   _   ||  |  | ||       ||  |_|  ||   _   ||  |  | |\n");
+    printf("|  |_|  ||  |_|  ||   |_| ||    ___||       ||  |_|  ||   |_| |\n");
+    printf("|       ||       ||       ||   | __ |       ||       ||       |\n");
+    printf("|       ||       ||       ||   | __ |       ||       ||       |\n");
+    printf("|       ||       ||  _    ||   ||  ||       ||       ||  _    |\n");
+    printf("|   _   ||   _   || | |   ||   |_| || ||_|| ||   _   || | |   |\n");
+    printf("|__| |__||__| |__||_|  |__||_______||_|   |_||__| |__||_|  |__|n");
+    printf("\n");
+    printf("                      |***********************|\n");
+    printf("                      |       MAIN  MENU      |\n");
+    printf("                      |***********************|\n");
+    printf("                      |          PLAY         |\n");
+    printf("                      |         ADD WORD      |\n");
+    printf("                      |***********************|\n");
+    printf("\n");
+    Sentence input, semuatebakan, tebakan;
+    CreateSentence(&input);
+    CreateSentence(&semuatebakan);
+    CreateSentence(&tebakan);
+    TabKata listKata; MakeEmpty(&listKata);
+    printf("Choose Menu: ");
+    START();
+    convertToArrayOfKata(&input,4);
+    int length;
+    int i = 0;
+    boolean guessed;
+    if((Eqstr(input.buffer[0].TabWord,"PLAY")) || (Eqstr(input.buffer[0].TabWord,"ADD")  && (Eqstr(input.buffer[1].TabWord, "WORD"))))
+    {
+        if (Eqstr(input.buffer[0].TabWord,"PLAY"))
+        {
+            Load("kata.txt",&listKata);
+            int kesempatan = 10;
+            srand(time(NULL));
+            Word soal = listKata.TK[rand()%10];
+            while (kesempatan != 0)
+            {
+                guessed = false;
+                printf("Tebakan sebelumnya: %s",semuatebakan.buffer[0].TabWord); // masukkin input stlahnya cara nampilin - gman ya
+                printf("\n");
+                printf("Kesempatan: %d\n",kesempatan);
+                Word kosong;
+                
+                for (i = 0; i < soal.Length; i++)
+                {
+                    kosong.TabWord[i] = '_';
+                }
+                kosong.Length = soal.Length;
+                for (i = 0; i < kosong.Length; i++)
+                {
+                    printf("%c ",kosong.TabWord[i]); // menampilkan _ _ _ _
+                }
+                printf("\n");
+                printf("Masukkan Tebakan: ");
+                CreateSentence(&tebakan);
+                START();
+                convertToArrayOfKata(&tebakan,1); //input satu huruf tebakan
+                for (i = 0; i < soal.Length;i++)
+                {
+                    if (tebakan.buffer[0].TabWord[0] == soal.TabWord[i])
+                    {
+                        kosong.TabWord[i] = soal.TabWord[i];
+                        printf("%c",kosong.TabWord[i]);
+                        guessed = true;
+                    }
+                } 
+                if (!guessed)
+                {
+                    kesempatan -= 1;
+                }
+                if (Eqstr(kosong.TabWord,soal.TabWord))
+                {
+                    printf("Berhasil menebak kata %s! Kamu mendapatkan %d poin.\n",soal.TabWord,soal.Length);
+                    soal = listKata.TK[rand()%10];
+                    CreateSentence(&semuatebakan);
+                }
+                // if (&tebakan == soal.TabWord[i]) // cara akses satu persatu hurufny gmana ya 
+                // {
+                //     printf("%c",soal.TabWord[i]); // nampilin huruf yang uda ketebak 
+                //     // cek apakah uda sama semua hurufnya
+                //     if (!Eqstr(&tebakan,soal.TabWord[i]))
+                //     {
+                //         printf("Berhasil menebak kata %s",&soal,"Kamu mendapatkan %d ", length, " poin");
+                //     }
+                // }
+            }
+        }
+        else if((Eqstr(input.buffer[0].TabWord, "ADD")) && (Eqstr(input.buffer[1].TabWord, "WORD")))
+        {
+            Load("kata.txt",&listKata);
+            tambahkata(&listKata);
+            Save("kata.txt", listKata);
+        }
+    }
+    else
+    {
+        printf("\nCommand tidak dikenali, silahkan masukkan command yang valid.\n");
+        printf("Choose Menu: ");
+        START();
+        convertToArrayOfKata(&input,4);
+    }
+}
 int main() {
   hangman();
   return 0;
