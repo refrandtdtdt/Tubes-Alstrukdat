addf97f86230236cc667a4fb8c865ca9f61588c1
Ferdeh <16521246@mahasiswa.itb.ac.id>
2022-12-02 21:02:27 +0700

    push

--- _src/Driver ADT/driver_linkedlist.c
+++ _src/Driver ADT/driver_linkedlist.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-#include "../ADT/stack_modif.h"
+#include "../ADT/point.h"
+#include "../ADT/linkedlist.h"
 #include "../ADT/mesinkata_modif.h"
 #include "../ADT/mesinkar.h"
 #include "../ADT/functions.h"
 
 int main () {
+    ListPoint tes; CreateEmptyListPoint(&tes);
+    Point titik = MakePoint(1,1);
+    InsVFirst(&tes,titik);
+    Point titik2 = MakePoint(1,2);
+    InsVLast(&tes,titik2);
+    PrintInfo(tes);
 
     return 0;
 }
